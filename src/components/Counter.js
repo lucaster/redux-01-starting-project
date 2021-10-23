6842453585661b26f0ab59578c47c75f4ae3d03c
Luca <luca.cavagnoli@gmail.com>
2021-10-23 10:59:55 +0200

    components interaction with store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  // sets up a subscription
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const handleIncrement = (clickEvent) => {
+    dispatch({
+      type: 'increment',
+    });
+  };
+
+  const handleDecrement = (clickEvent) => {
+    dispatch({
+      type: 'decrement',
+    });
+  };
+
+  const handleToggleCounter = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
+      <button onClick={handleToggleCounter}>Toggle Counter</button>
     </main>
   );
 };
